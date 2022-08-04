d1ec6bc87aeb188d4cfb1bb3526a71bb15e66a67
Mateo Ormeño <mateoormeno@MacBook-Pro-de-Mateo.local>
2022-08-04 02:28:55 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
+        <p style={{color: 'gold', fontSize: '50px'}}>
+          mi nombre es Mateo
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{fontSize: '5px'}}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't Click
         </a>
       </header>
     </div>
